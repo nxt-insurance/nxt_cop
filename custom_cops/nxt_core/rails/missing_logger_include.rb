f7370683f74967fd570d8a02f9447a394d01c464
shalvah-gs <s.adebayo@hellogetsafe.com>
2024-02-16 16:05:33 +0100

    Fix false positives in MissingLoggerInclude cop

--- custom_cops/nxt_core/rails/missing_logger_include.rb
+++ custom_cops/nxt_core/rails/missing_logger_include.rb
@@ -16,8 +16,26 @@ module CustomCops
         MSG = 'Missing `include Nxt::Loggable` for a class which uses the logger.'
 
         # NodePattern docs: https://github.com/rubocop/rubocop-ast/blob/master/docs/modules/ROOT/pages/node_pattern.adoc
+        # Test at https://nodepattern.herokuapp.com/
         def_node_matcher :includes_nxt_loggable?, <<~PATTERN
           (begin <(send nil? :include (const (const nil? :Nxt) :Loggable)) ...>)
+        PATTERN
+
+        def_node_matcher :includes_nxt_core_loggable?, <<~PATTERN
+          (begin <(send nil? :include (const (const (const nil? :NxtCore) :Logging) :Loggable)) ...>)
+        PATTERN
+
+        def_node_matcher :is_maintenance_task?, <<~PATTERN
+          (class
+             (const ...) # Class name
+             (const (const {nil? | (cbase)} :MaintenanceTasks) :Task) # MaintenanceTasks::Task or ::MaintenanceTasks::Task
+          ...)
+        PATTERN
+
+        def_node_matcher :is_controller?, <<~PATTERN
+          (class
+             (const {nil? | (cbase)} /.+Controller/) # Class name ending in "Controller"
+          ...)
         PATTERN
 
         def on_send(node)
@@ -26,11 +44,16 @@ module CustomCops
           class_node = node.ancestors.find(&:class_type?)
           return unless class_node
 
-          class_definition_node = class_node.each_child_node(:begin).first
-          return if includes_nxt_loggable?(class_definition_node)
+          # Controllers and maintenance tasks already include the Loggable module
+          return if is_maintenance_task?(class_node) || is_controller?(class_node)
+
+          includes = class_node.each_child_node(:begin).first
+          return if includes_nxt_loggable?(includes) || includes_nxt_core_loggable?(includes)
 
           add_offense(class_node, message: MSG) do |corrector|
-            corrector.insert_before(class_definition_node.child_nodes.first, "include Nxt::Loggable\n")
+            if includes
+              corrector.insert_before(includes.child_nodes.first, "include Nxt::Loggable\n")
+            end
           end
         end
       end
