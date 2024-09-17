2ceeb7461c11c4fc6763fc522d8650393209ba27
Akihiko Ito <a.ito@hellogetsafe.com>
2024-09-17 10:22:34 +0200

    Configuration for standard

--- custom_cops/nxt_core/rails/use_of_rails_logger.rb
+++ custom_cops/nxt_core/rails/use_of_rails_logger.rb
@@ -9,13 +9,13 @@ module CustomCops
 
         RESTRICT_ON_SEND = [:logger].freeze
 
-        MSG = 'Use of Rails.logger is not allowed. Use `logger` and include `Nxt::Loggable`'
+        MSG = "Use of Rails.logger is not allowed. Use `logger` and include `Nxt::Loggable`"
 
         def on_send(node)
           return unless node.receiver&.const_type? && node.receiver.short_name == :Rails
 
           add_offense(node, message: MSG) do |corrector|
-            corrected = node.source.gsub('Rails.logger', 'logger')
+            corrected = node.source.gsub("Rails.logger", "logger")
             corrector.replace(node, corrected)
           end
         end
